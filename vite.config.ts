6dced18e1182c834a3193e105d91467bd8995c04
Ian <yiheng.me@gmail.com>
2022-05-23 03:59:37 +0200

    setup

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import eslintPlugin from 'vite-plugin-eslint';
+import stylelintPlugin from 'vite-plugin-stylelint';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// 插件会在运行开发和构建时执行
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    // https://github.com/aleclarson/vite-tsconfig-paths
+    tsconfigPaths(),
+    eslintPlugin({
+      // https://github.com/gxmari007/vite-plugin-eslint
+      cache: false,
+    }),
+    stylelintPlugin({
+      // https://github.com/ModyQyW/vite-plugin-stylelint
+      cache: false,
+    }),
+  ],
+});
