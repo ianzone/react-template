e8264847e4e83e45344ad982d247cca8d74cac32
ian <yihengz@outlook.com>
2024-08-02 17:07:35 +0800

    test: 💍 useCounter

--- src/hooks/useCounter.test.ts
+++ src/hooks/useCounter.test.ts
@@ -1,14 +1,23 @@
-import { renderHook } from '@testing-library/react';
-import { act } from 'react';
-import { describe, expect, it } from 'vitest';
+import { act, renderHook } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
 import { useCounter } from './useCounter';
 
 describe('useCounter', () => {
-	it('should increment counter', () => {
+	test('should render the initial count', () => {
 		const { result } = renderHook(useCounter);
-		act(() => {
-			result.current.increment();
-		});
+		expect(result.current.count).toBe(0);
+	});
+
+	test('should increment counter', () => {
+		const { result } = renderHook(useCounter);
+		act(() => result.current.increment());
 		expect(result.current.count).toBe(1);
+	});
+
+	test('should start count from 10', () => {
+		const { result } = renderHook(useCounter, {
+			initialProps: 10,
+		});
+		expect(result.current.count).toBe(10);
 	});
 });
