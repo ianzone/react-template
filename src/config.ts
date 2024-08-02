803e3c717a7e66bafbd4dfb5f8f2e3ecab25774d
ian <yihengz@outlook.com>
2024-08-02 10:01:36 +0800

    feat: 🎸 configs

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-const whiteLabel1 = {
-	name: 'whiteLabel1',
-	description: 'whiteLabel1 description',
-};
-
-export const brand = whiteLabel1;
---
+++ src/configs/index.ts
@@ -0,0 +1,16 @@
+import { brand1 } from './brand1';
+import { brand2 } from './brand2';
+import { whiteLabel } from './white-label';
+
+function getConfig() {
+	const brands = [brand1, brand2];
+
+	for (const brand of brands) {
+		if (brand.domains.find((item) => item === window.location.host)) {
+			return brand;
+		}
+	}
+	return whiteLabel;
+}
+
+export default getConfig();
