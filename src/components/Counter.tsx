9cca2e973f5a667b2524b64e1b686481a7607a4b
ian <yihengz@outlook.com>
2024-08-06 09:14:57 +0800

    refactor: 💡 counter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,23 +2,23 @@ import type { CSSProperties, HTMLAttributes } from 'react';
 import { useCounter } from 'src/hooks';
 
 interface CounterProps extends HTMLAttributes<HTMLDivElement> {
-	init?: number;
+  init?: number;
 }
 
 export function Counter(props: CounterProps) {
-	const { count, increment } = useCounter(props.init);
+  const { count, increment } = useCounter(props.init);
 
-	const { style } = props;
-	return (
-		<div style={{ ...css, ...style }}>
-			<h1>{count}</h1>
-			<button type='button' onClick={() => increment()}>
-				Increment
-			</button>
-		</div>
-	);
+  const { style } = props;
+  return (
+    <div style={{ ...css, ...style }}>
+      <h1>{count}</h1>
+      <button type='button' onClick={() => increment()}>
+        Increment
+      </button>
+    </div>
+  );
 }
 
 const css: CSSProperties = {
-	backgroundColor: 'grey',
+  backgroundColor: 'grey',
 };
