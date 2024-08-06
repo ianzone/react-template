d6e94146190d42a7d3893f48598eaf2acc9341c0
ian <yihengz@outlook.com>
2024-08-06 08:44:59 +0800

    refactor: 💡 counter test

--- src/components/Counter.test.tsx
+++ src/components/Counter.test.tsx
@@ -1,23 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import user from '@testing-library/user-event';
 import { describe, expect, test } from 'vitest';
 import { Counter } from './Counter';
 
 describe('Counter', () => {
-	test('renders a count of 0', () => {
-		render(<Counter />);
-		expect(screen.getByRole('heading').textContent).toBe('0');
-	});
+  test('renders a count of 0', () => {
+    const { getByRole } = render(<Counter />);
+    expect(getByRole('heading').textContent).toBe('0');
+  });
 
-	test('renders a count of 1', () => {
-		render(<Counter init={1} />);
-		expect(screen.getByRole('heading').textContent).toBe('1');
-	});
+  test('renders a count of 1', () => {
+    const { getByRole } = render(<Counter init={1} />);
+    expect(getByRole('heading').textContent).toBe('1');
+  });
 
-	test('renders a count of 1 after clicking the increment button', async () => {
-		render(<Counter />);
-		const incrementButton = screen.getByRole('button', { name: 'Increment' });
-		await user.click(incrementButton);
-		expect(screen.getByRole('heading').textContent).toBe('1');
-	});
+  test('renders a count of 1 after clicking the increment button', async () => {
+    const { getByRole } = render(<Counter />);
+    const incrementButton = getByRole('button', { name: 'Increment' });
+    await user.click(incrementButton);
+    expect(getByRole('heading').textContent).toBe('1');
+  });
 });
