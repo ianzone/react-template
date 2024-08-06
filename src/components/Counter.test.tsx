a2699f62dd72d063a7803096658638e4872f01eb
ian <yihengz@outlook.com>
2024-08-06 09:18:10 +0800

    test: 💍 test

--- src/components/Counter.test.tsx
+++ src/components/Counter.test.tsx
@@ -4,12 +4,12 @@ import { describe, expect, test } from 'vitest';
 import { Counter } from './Counter';
 
 describe('Counter', () => {
-  test('renders a count of 0', () => {
+  test('initial count should be 0', () => {
     const { getByRole } = render(<Counter />);
     expect(getByRole('heading').textContent).toBe('0');
   });
 
-  test('renders a count of 1', () => {
+  test('initial count should be set to 1', () => {
     const { getByRole } = render(<Counter init={1} />);
     expect(getByRole('heading').textContent).toBe('1');
   });
